42bd0886288913da106a72a76013a319c18b8299
gsarig <contact@gsarigiannidis.gr>
2024-11-30 20:02:55 +0200

    Adds support for basic calculations

--- src/utils/stringUtils.ts
+++ src/utils/stringUtils.ts
@@ -1,7 +1,29 @@
-export function replacePlaceholders(content: string, properties: Record<string, string>): string {
-	for (const key in properties) {
-		const placeholderRegex = new RegExp(`\\{\\{\\$${key}\\}\\}`, 'g');
-		content = content.replace(placeholderRegex, properties[key]);
+export function replacePlaceholders(content: string, properties: Record<string, string | number>): string {
+	function evaluateExpression(expression: string, vars: Record<string, string | number>): string {
+
+		// Substitute variables with their values in the expression.
+		const substitutedExpr = expression.replace(/\b(\w+)\b/g, match => {
+			const value = vars[match];
+			return typeof value !== 'undefined' ? value.toString() : match;
+		});
+
+		// Clean the expression to avoid syntax errors.
+		const cleanedExpr = substitutedExpr.replace(/\s+/g, ' ').trim();
+
+		try {
+			// Evaluate only if the expression seems like a calculation.
+			if (/[\d+\-*/]/.test(cleanedExpr)) {
+				return new Function(`return (${cleanedExpr});`)().toString();
+			}
+		} catch (error) {
+			// Do nothing if the evaluation fails.
+		}
+		// Return the substituted expression if not evaluable.
+		return substitutedExpr;
 	}
-	return content;
+
+	// noinspection RegExpRedundantEscape
+	const placeholderRegex = /\{\{\$(.+?)\}\}/g;
+
+	return content.replace(placeholderRegex, (_, expr) => evaluateExpression(expr, properties));
 }
