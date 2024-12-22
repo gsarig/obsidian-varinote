c87b625c13c30ecec66c3ccde755d2d20b94705a
gsarig <contact@gsarigiannidis.gr>
2024-12-22 12:13:27 +0200

    Fixes "Casting to any should be avoided as much as possible" No additional type casting is probably needed for `this.app`

--- src/utils/templateUtils.ts
+++ src/utils/templateUtils.ts
@@ -1,7 +1,7 @@
 import {SettingPlugin} from '../types/plugin-settings';
 
 export function getTemplateFolderPath(): string | null {
-	const settingPlugin = (this.app as any).internalPlugins.plugins['templates'] as SettingPlugin;
+	const settingPlugin = this.app.internalPlugins.plugins['templates'] as SettingPlugin;
 	if (settingPlugin && settingPlugin.enabled) {
 		return settingPlugin.instance.options.folder;
 	}
